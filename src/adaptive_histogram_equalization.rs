d6ea1c6ffc30c889c133fa10c18acb1f17fe779a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-26 00:53:38 +0100

    SIMD reduce sum

--- src/adaptive_histogram_equalization.rs
+++ src/adaptive_histogram_equalization.rs
@@ -1,3 +1,11 @@
+use core::simd::{cmp::SimdPartialOrd, num::SimdUint, u8x64, u32x64, mask32x64};
+const ZERO : u32x64  = u32x64::from_array([0; _]);
+//const INDEX: u8x64 = u8x64::from_array(std::array::from_fn(|i| i as u8));
+const INDEX: u8x64 = u8x64::from_array([
+0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
+0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,
+0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,
+0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x3A,0x3B,0x3C,0x3D,0x3E,0x3F]);
 use {vector::{xy, minmax, MinMax}, image::{Image, XYZ}};
 pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, radius: u32) -> Image<Box<[u32]>> { // TODO: SIMD
 	let radius = radius as i32;
@@ -8,7 +16,7 @@ pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, ra
 	//assert!(image.size.x <= image.size.y); // FIXME: benchmark compute vs bandwidth tradeoff
 	assert!(image.size.x as usize*(coarse*fine)*4 <= 32*1024*1024);
 	let luminance = image.as_ref().map(|XYZ{Y,..}| (f32::ceil((Y-min)/(max-min)*(Nbins as f32)-1.)) as u16);
-	struct Histogram { sums: core::simd::u32x64, bins: [core::simd::u32x64; coarse] }
+	struct Histogram { sums: u32x64, bins: [u32x64; coarse] }
 	let mut columns = unsafe{Box::<[Histogram]>::new_zeroed_slice(luminance.size.x as usize).assume_init()};
 	let mut rank = Image::zero(luminance.size);
 	let ([w, h], stride) = (luminance.size.signed().into(), luminance.stride as i32);
@@ -24,7 +32,7 @@ pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, ra
 	loop {
 		if !(y < h) { break; }
 		println!("{y}");
-		let Histogram{mut sums, mut bins} = Histogram{sums: [0; _].into(), bins: [[0; _].into(); _]};
+		let Histogram{mut sums, mut bins} = Histogram{sums: ZERO, bins: [ZERO; _]};
 		for x in -radius..=radius { 
 			let ref column = columns[x.max(0) as usize];
 			sums += column.sums;
@@ -33,7 +41,9 @@ pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, ra
 		for x in 0..w-1 {
 			let index = (y*stride+x) as usize;
 			let bin = luminance[index] as usize;
-			rank[index] = sums[0..bin/fine].iter().sum::<u32>()+bins[bin/fine][0..=bin%fine].iter().sum::<u32>();
+			rank[index] = // Lookup masks?
+				mask32x64::from(INDEX.simd_lt(u8x64::splat((bin/fine) as u8))).select(sums, ZERO).reduce_sum() + 
+				mask32x64::from(INDEX.simd_le(u8x64::splat((bin%fine) as u8))).select(bins[bin/fine], ZERO).reduce_sum();
 			// Slide right
 			let ref right = columns[(x+radius+1).min(w-1) as usize];
 			let ref left = columns[(x-radius).max(0) as usize];
@@ -70,7 +80,9 @@ pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, ra
 		for x in (1..w).into_iter().rev() {
 			let index = (y*stride+x) as usize;
 			let bin = luminance[index] as usize;
-			rank[index] = sums[0..bin/fine].iter().sum::<u32>()+bins[bin/fine][0..=bin%fine].iter().sum::<u32>();
+			rank[index] = // Lookup masks?
+				mask32x64::from(INDEX.simd_lt(u8x64::splat((bin/fine) as u8))).select(sums, ZERO).reduce_sum() + 
+				mask32x64::from(INDEX.simd_le(u8x64::splat((bin%fine) as u8))).select(bins[bin/fine], ZERO).reduce_sum();
 			// Slide left
 			let ref left = columns[(x-radius-1).max(0) as usize];
 			let ref right = columns[(x+radius).min(w-1) as usize];
