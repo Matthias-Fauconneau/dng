ae560bd5fa825025303fca884d07a7c295433e7b
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-25 19:44:03 +0100

    bring back adaptive histogram equalization

---
+++ src/adaptive_histogram_equalization.rs
@@ -0,0 +1,70 @@
+use {vector::{xy, minmax, MinMax}, image::{Image, XYZ}};
+
+pub fn adaptive_histogram_equalization(image: &Image<impl AsRef<[XYZ<f32>]>>, radius: u32) -> Image<Box<[u32]>> {
+	let MinMax{min, max} = minmax(image.data.as_ref().iter().map(|&XYZ{Y,..}| Y)).unwrap();
+	let bins = 0x800; // TODO: multilevel histogram, SIMD
+	assert!(image.size.x as usize*bins as usize*2 <= 64*1024*1024);
+	let luminance = image.as_ref().map(|XYZ{Y,..}| (f32::ceil((Y-min)/(max-min)*(bins as f32)-1.)) as u16);
+	assert!(733 <= radius && radius <= 1024);
+	assert!((radius+1+radius) as usize >= bins, "{} {bins}", radius+1+radius);
+	let radius = radius as i32;
+	let mut column_histograms = vec![vec![0; bins as usize]; luminance.size.x as usize]; // ~120M. More efficient to slide : packed add vs indirect scatter add
+	let mut rank = Image::<Box<[u32]>>::zero(luminance.size);
+	for y in -radius..=radius { for x in 0..luminance.size.x { column_histograms[x as usize][luminance[xy{x,y: y.max(0) as u32}] as usize] += 1; } }
+	let [w, h] = luminance.size.signed().into();
+	let stride = luminance.stride as i32; // Slightly less verbose sign casting
+	assert_eq!(rank.stride as i32, stride);
+	let start = std::time::Instant::now();
+	let mut y = 0;
+	loop {
+		if !(y < h) { break; }
+		println!("{y}");
+		let mut histogram = vec![0; bins as usize];
+		for x in -radius..=radius { for bin in 0..bins { histogram[bin as usize] += column_histograms[x.max(0) as usize][bin as usize]; } }
+		for x in 0..w-1 {
+			let luminance = luminance[(y*stride+x) as usize];
+			rank[(y*stride+x) as usize] = histogram[0..=luminance as usize].iter().sum();
+			// Slide right
+			for bin in 0..bins { histogram[bin as usize] = (histogram[bin as usize] as i32
+				+ column_histograms[(x+radius+1).min(w-1) as usize][bin as usize] as i32
+				- column_histograms[(x-radius).max(0) as usize][bin as usize] as i32) as u32;
+			}
+		}
+		{ // Last of row iteration (not sliding further right after)
+			let x = w-1;
+			let luminance = luminance[(y*stride+x) as usize];
+			rank[(y*stride+x) as usize] = histogram[0..=luminance as usize].iter().sum();
+		}
+		// Slide down
+		for x in 0..w {
+			column_histograms[x as usize][luminance[((y-radius).max(0)*stride+x) as usize] as usize] -= 1;
+			column_histograms[x as usize][luminance[((y+radius+1).min(h-1)*stride+x) as usize] as usize] += 1;
+		}
+		y += 1;
+		if !(y < h) { break; }
+		let mut histogram = vec![0; bins as usize];
+		for x in (w-1)-radius..=(w-1)+radius { for bin in 0..bins { histogram[bin as usize] += column_histograms[x.min(w-1) as usize][bin as usize]; } }
+		for x in (1..w).into_iter().rev() {
+			let luminance = luminance[(y*stride+x) as usize];
+			rank[(y*stride+x) as usize] = histogram[0..=luminance as usize].iter().sum();
+			// Slide left
+			for bin in 0..bins { histogram[bin as usize] = (histogram[bin as usize] as i32
+				+ column_histograms[(x-radius-1).max(0) as usize][bin as usize] as i32
+				- column_histograms[(x+radius).min(w-1) as usize][bin as usize] as i32) as u32;
+			}
+		}
+		{ // Back to first of row iteration (not sliding further left after)
+			let x = 0;
+			let luminance = luminance[(y*stride+x) as usize];
+			rank[(y*stride+x) as usize] = histogram[0..=luminance as usize].iter().sum();
+		}
+		// Slide down
+		for x in 0..w {
+			column_histograms[x as usize][luminance[((y-radius).max(0)*stride+x) as usize] as usize] -= 1;
+			column_histograms[x as usize][luminance[((y+radius+1).min(h-1)*stride+x) as usize] as usize] += 1;
+		}
+		y += 1;
+	}
+	println!("{}ms", start.elapsed().as_millis());
+	rank
+}
--- src/dng.rs
+++ src/dng.rs
@@ -1,6 +1,17 @@
-mod gain; use gain::*;
 pub fn default<T: Default>() -> T { Default::default() }
-use vector::{xy, uint2, vec2, mulv, min, minmax, MinMax};
+
+mod vector_uv { vector::vector!(2 uv T T, u v, U V); } pub use vector_uv::uv;
+const fn uv(XYZ{X,Y,Z}: XYZ<f32>) -> uv<f32> { let d = X + 15.*Y + 3.*Z; uv{u: 4.*X/d, v: 9.*Y/d} }
+const D50 : XYZ<f32> = XYZ{X: 0.964212, Y: 1., Z: 0.825188};
+const uv_D50 : uv<f32> = uv(D50); 
+fn to_uv_D50(XYZ: XYZ<f32>) -> uv<f32> { uv(XYZ)-uv_D50 }
+fn XYZ(uv{u,v}: uv<f32>, Y: f32) -> XYZ<f32> { XYZ{X: (9.*u)/(4.*v)*Y, Y, Z: (12.-3.*u-20.*v)/(4.*v)*Y} }
+fn from_uv_D50(uv: uv<f32>, Y: f32) -> XYZ<f32> { XYZ(uv_D50+uv, Y) }
+
+mod gain; use gain::*;
+mod adaptive_histogram_equalization; use adaptive_histogram_equalization::*;
+
+use vector::{xy, uint2, vec2, mulv, min};
 use image::{Image, XYZ, rgb, rgbf, rgba8, bilinear_sample, blur_3, oetf8_12, sRGB8_OETF12};
 use rawler::{rawsource::RawSource, get_decoder, RawImage, RawImageData, Orientation, decoders::{Camera, WellKnownIFD}, formats::tiff::{Entry, Value}};
 use rawler::{imgop::xyz::Illuminant::D65, tags::DngTag::OpcodeList2};
@@ -10,84 +21,47 @@ pub fn load(path: impl AsRef<std::path::Path>) -> Result<Image<Box<[rgba8]>>, Bo
 	let decoder = get_decoder(file)?;
 	let RawImage{whitelevel, data: RawImageData::Integer(data), width, height, wb_coeffs: rcp_as_shot_neutral, orientation, camera: Camera{forward_matrix, ..}, ..}
 		= decoder.raw_image(file, &default(), false)? else {unimplemented!()};
+	let cfa = Image::new(xy{x: width as u32, y: height as u32}, data);
 	let tags = decoder.ifd(WellKnownIFD::VirtualDngRawTags)?.unwrap();
 	let &[white_level] = whitelevel.0.as_array().unwrap();
 	let as_shot_neutral = 1./rgbf::from(*rcp_as_shot_neutral.first_chunk().unwrap()); // FIXME: rawler: expose non-inverted
-	let Some(Entry{value: Value::Undefined(code), ..}) = tags.get_entry(OpcodeList2) else {panic!()};
-	let gain = gain(code);
 	let ref forward = forward_matrix[&D65]; // darktable has a dedicated color temperature (chromatic adaptation) much later in the pipeline
 	let forward = *forward.as_array::<{3*3}>().unwrap().map(|v| v.try_into().unwrap()).as_chunks().0.as_array().unwrap();
 	let forward = forward.map(rgbf::from)
 		.map(|row| row / as_shot_neutral) // ~ * 1/D
 		.map(|row| row / white_level as f32)
 		.map(<[_;_]>::from);
-	let cfa = Image::new(xy{x: width as u32, y: height as u32}, data);
-	let scale = vec2::from(gain[0].size-uint2::from(1))/vec2::from(cfa.size/2-uint2::from(1))-vec2::from(f32::EPSILON);
-	let image = Image::from_xy(cfa.size/2, |p| {
-		let [b, g10, g01, r] = [(0,[0,0]),(1,[1,0]),(2,[0,1]),(3,[1,1])].map(|(i,[x,y])| bilinear_sample(&gain[i], scale*vec2::from(p)) * f32::from(cfa[p*2+xy{x,y}]));
+	let Some(Entry{value: Value::Undefined(code), ..}) = tags.get_entry(OpcodeList2) else {panic!()};
+	let gain = gain(code);
+	let gain_map_scale = vec2::from(gain[0].size-uint2::from(1))/vec2::from(cfa.size/2-uint2::from(1))-vec2::from(f32::EPSILON);
+	// Demosaic
+	let mut image = Image::from_xy(cfa.size/2, |p| {
+		let [b, g10, g01, r] = [(0,[0,0]),(1,[1,0]),(2,[0,1]),(3,[1,1])].map(|(i,[x,y])| bilinear_sample(&gain[i], gain_map_scale*vec2::from(p)) * f32::from(cfa[p*2+xy{x,y}]));
 		mulv(forward, [r, (g01+g10)/2., b]).into()
 	});
-	let pixel_count = image.data.len() as u32;
+	// Dehaze
 	let haze = blur_3::<512>(&image);
 	let min = min(image.data.iter().zip(&haze.data).map(|(image, haze)| image/haze)).unwrap(); // ~ 1/4
-	fn xy(XYZ{X,Y,Z}: XYZ<f32>) -> xy<f32> { xy{x: X/(X+Y+Z), y: Y/(X+Y+Z)} }
-	let xy = image.as_ref().map(|&XYZ| xy(XYZ));
-	let image = Image::from_iter(image.size, image.data.iter().zip(haze.data).map(|(image, haze)| image-min*haze)); // Scale haze correction to avoid negative values
-	let MinMax{min, max} = minmax(image.data.iter().copied()).unwrap();
-	let bins = 0x10000;
-	let mut histogram = vec![0; bins];
-	for &XYZ{Y,..} in &image.data { histogram[f32::ceil((Y-min.Y)/(max.Y-min.Y)*bins as f32-1.) as usize] += 1; }
-	let cdf = histogram.into_iter().scan(0, |a, h| { *a += h; Some(*a) }).collect::<Box<[u32]>>();
-	assert_eq!(cdf[0xFFFF], pixel_count);
-	let oetf = &sRGB8_OETF12;
-	const D50 : XYZ<f32> = XYZ{X: 0.964212, Y: 1., Z: 0.825188};
-	let n = D50;
-	//fn f(t: f32) -> f32 { const δ : f32 = 6./29.; if t > δ.powi(3) { 1.16*t.cbrt()-0.16 } else { t/(δ/2.).powi(2) } }
-	mod vector_uv { vector::vector!(2 uv T T, u v, U V); } pub use vector_uv::uv;
-	fn uv(XYZ{X,Y,Z}: XYZ<f32>) -> uv<f32> { let d = X + 15.*Y + 3.*Z; uv{u: 4.*X/d, v: 9.*Y/d} }
-	//dbg!(uv(n));
-	//let image = image.map(|XYZ@XYZ{Y,..}| {
-	let image = Image::from_iter(image.size, image.data.into_iter().zip(xy.data).map(|(XYZ@XYZ{Y,..},_/*xy{x,y}*/)| {
-		let rank = cdf[f32::ceil((Y-min.Y)/(max.Y-min.Y)*bins as f32-1.) as usize];
-		let L = rank as f32 / pixel_count as f32;
+	image.mut_zip_map(&haze, |image, haze| image-min*haze); // Scales haze correction to avoid negative values
+	// Adaptive Histogram Equalization
+	let radius = 1024; //(std::cmp::min(image.size.x, image.size.y) - 1) / 2;
+	let rank = adaptive_histogram_equalization(&image, radius);
+	assert_eq!(image.stride, rank.stride);
+	image.mut_zip_map(&rank, |XYZ@XYZ{Y,..}, &rank| {
+		let L = rank as f32 / (radius+1+radius).pow(2) as f32;
 		assert!(L >= 0. && L <= 1.);
 		let XYZ@XYZ{Y,..} = if Y > 0. { L.powi(3)*XYZ/Y } else { XYZ };
 		assert!(Y >= 0. && Y <= 1.);
-		//XYZ{X: f*x, Y: f*y, Z: f*z}
-
-		//let L = f(Y/n.Y);
-		let uv_n = uv(n);
-		let uv = /*1.3*L*(*/uv(XYZ)-uv_n;//);
-		//let uv{u,v} = uv;
-		//let C = sqrt(u*u+v*v);
-		//let s = C/L;
-		//let C = 2.*C;
-		let uv = 2.*uv;
-		//fn xy(uv{u, v}: uv<f32>) -> xy<f32> { let d = 6.*u - 16.*v + 12.; xy{x: 9.*u/d, y: 4.*v/d} }
-		let uv{u,v} = uv_n+uv;
-		XYZ{X: (9.*u)/(4.*v)*Y, Y, Z: (12.-3.*u-20.*v)/(4.*v)*Y}
-		/*//let D65 = XYZ{X: 0.950489, Y: 1., Z: 1.088840};
-		fn f(t: f32) -> f32 { const δ : f32 = 6./29.; if t > δ.powi(3) { t.cbrt() } else { 4./29. + t/(3.*δ.powi(2)) } }
-		let L = 1.16*f(Y/n.Y)-0.16;
-		let a = 5.*(f(X/n.X)-f(Y/n.Y));
-		let b = -2.*(f(Z/n.Z)-f(Y/n.Y));
-		let C = sqrt(a*a+b*b);*/
-		/*let [dx, dy] = [x-x0, y-y0];
-		let r = sqrt(sq(dx)+sq(dy));
-		let s = 1./12.;
-		let [x, y] = [x0+s*dx/r, y0+s*dy/r];
-		let z = 1.-x-y;*/
-	}));
-
-	// FIXME: linear XYZ export
-
-	//let mean = image.data.iter().copied().sum::<XYZ<f32>>() / image.data.len() as f32;
-	let scale = 1.; //(1./2.)*XYZ::<f32>::from(image.data.len() as f64 / image.data.iter().map(|&XYZ| XYZ::<f64>::from(XYZ)).sum::<XYZ<f64>>());
+		from_uv_D50(2.*to_uv_D50(XYZ), Y)
+	});
+	// Linear sRGB
 	const sRGB_from_XYZD50 : [[f32; 3]; 3] = [[3.1338561, -1.6168667, -0.4906146], [-0.9787684, 1.9161415, 0.0334540], [0.0719450, -0.2289914, 1.4052427]];
-	let image = image.map(|XYZ| rgb::from(mulv(sRGB_from_XYZD50, <[_; _]>::from(scale*XYZ))));
-
+	let image = image.map(|XYZ| rgb::from(mulv(sRGB_from_XYZD50, <[_; _]>::from(XYZ))));
+	// Export
+	// FIXME: copy Orientation tag
 	std::fs::write("../sRGB.linear.jxl", &*jpegxl_rs::encoder_builder().build()?.encode::<_,f32>(&bytemuck::cast_slice::<_,f32>(&image.data), image.size.x, image.size.y)?)?;
-
+	// View
+	let oetf = &sRGB8_OETF12;
 	let image = image.map(|rgb| rgba8::from(rgb.map(|c:f32| oetf8_12(oetf, c.clamp(0., 1.)))));
 
 	use Orientation::*;
@@ -96,10 +70,6 @@ pub fn load(path: impl AsRef<std::path::Path>) -> Result<Image<Box<[rgba8]>>, Bo
 		Rotate90 => Image::from_xy({let xy{x,y} = image.size; xy{x: y, y: x}}, |xy{x,y}| image[xy{x: y, y: image.size.y-1-x}]),
 		o => { eprintln!("{o:#?}"); image },
 	};
-
-	/*use {zune_jpegxl::JxlSimpleEncoder, zune_core::{options::EncoderOptions, colorspace::ColorSpace, bit_depth::BitDepth}};
-	let rgb = image.data.iter().map(|&rgba{r,g,b,..}| [r,g,b].into_iter()).flatten().collect::<Box<_>>();
-	std::fs::write("../sRGB.8.jxl", JxlSimpleEncoder::new(&rgb, EncoderOptions::new(image.size.x as usize, image.size.y as usize, ColorSpace::RGB, BitDepth::Eight)).encode().unwrap())?;*/
 
 	Ok(image)
 }
